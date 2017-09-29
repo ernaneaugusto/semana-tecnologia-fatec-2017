78f4df16b35bd2e4c1d65226c831da8583c99ff1
ernaneaugusto <ernaneatcipriano@gmail.com>
2017-09-29 19:40:29 -0300

    add botao voltar ao topo

---
+++ src/js/topo.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+   $(window).scroll(function(){
+      var altura = $(window).height() * 3/4;
+      var i=0;
+
+      if ($(this).scrollTop() > altura) {
+         $('a[href="#top"]').fadeIn();
+      } else {
+         $('a[href="#top"]').fadeOut();
+      }
+   });
+
+   $('a[href="#top"]').click(function(){
+      $('html, body').animate({scrollTop : 0},900);
+      return false;
+   });
+});
